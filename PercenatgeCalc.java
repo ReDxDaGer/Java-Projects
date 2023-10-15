420456e2f3bcba6f35f4240e9bafb4106bbd0d1f
ReDxDaGer <yashmehrotra45@gmail.com>
2023-10-15 19:01:00 +0530

    percenateg Calc for stdnts

---
+++ PercenatgeCalc.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+//calculator for
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        System.out.println("Please enter your name below");
+        String name = S.nextLine();
+
+        System.out.println("PLease enter your age below");
+        int age = S.nextInt();
+
+        System.out.println("Your name is " + name + " and your age is " + age + " it is all saved inside our server now have a happy calculatinf");
+
+        System.out.print("Please enter your marks in maths : ");
+        byte maths = S.nextByte();
+
+        System.out.print("Please enter your marks in science : ");
+        byte science  = S.nextByte();
+
+        System.out.print("Please enter your marks in english : ");
+        byte english = S.nextByte();
+
+        System.out.print("Please enter your marks in Social Science : ");
+        byte socialScience = S.nextByte();
+
+        System.out.print("Please enter your marks in Hindi : ");
+        byte hindi = S.nextByte();
+
+        double sum  = maths + english + socialScience + hindi + science;
+
+        System.out.print("What is the total marks of all the five subjects : ");
+        double total = S.nextDouble();
+
+        double percentage = (sum/total)*100;
+
+        System.out.println(name + "your percenatge in CBSE board exams is " + percentage + "%");
+    }
+}
